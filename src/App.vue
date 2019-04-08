f4423aa494a1547fc693b6bd048d6610e5e3a7df
Mazen Al-Hagri <mazen.abdulaziz@gmail.com>
2019-04-08 08:58:58 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>  
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"><a class="navbar-brand" href="/">
-      <img style="width: 50px; background-color: white" src="./assets/logo-main.png">
+      <img style="width: 50px" src="./assets/logo-main.png">
        <span class="d-none d-sm-inline">&nbsp; Measles, Mumps and Rubella Database</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
