30c32dfb1908ca7691d7f5e9da7a24f9859db99b
Mazen Al-Hagri <mazen.abdulaziz@gmail.com>
2019-04-19 18:22:46 +0200

    UI changes

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <div>  
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"><a class="navbar-brand" href="/">
-      <img style="width: 50px" src="./assets/logo-main.png">
-       <span class="d-none d-sm-inline">&nbsp; Measles, Mumps and Rubella Database</span></a>
+       <span class="d-none d-sm-inline"><b>MMRdb</b> | Measles, Mumps, and Rubella Viruses Database and Analysis Resource</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
           </div>
       </div>
       <div class="card">
-          <img src="../assets/card.png" width="348" height="325" class="card-img-top">
+          <img src="../assets/blast.png" width="348" height="325" class="card-img-top">
           <div class="card-body">
           <h5 class="card-title hippy"><i class="fa fa-search"></i> BLAST Search</h5>
           <p class="card-text">Nucleotide Blast and Protein Blast.</p>
@@ -24,7 +24,7 @@
       </div>
 
       <div class="card">
-          <img src="../assets/card.png" width="348" height="325" class="card-img-top">
+          <img src="../assets/epi.png" width="348" height="325" class="card-img-top">
           <div class="card-body">
           <h5 class="card-title hippy"><i class="fa fa-dna"></i> Antigenic Epitopes</h5>
           <p class="card-text">B-cell epitopes, T-cell epitopes, linear epitopes and discontinuous epitopes.</p>
